5a9a0b4cc8afd810fbd7cc6d3b59bf1cdd224f32
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-05 22:19:44 +0900

    Title: 퀵 정렬 구현, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

--- 241105/퀵 정렬 구현/implement-quick-sort.java
+++ 241105/퀵 정렬 구현/implement-quick-sort.java
@@ -7,31 +7,33 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
         int[] ints = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i =0; i< n; i++){
+        for (int i = 0; i < n; i++) {
             ints[i] = Integer.parseInt(st.nextToken());
         }
-        quicksort(ints,0, n - 1);
+        quicksort(ints, 0, n - 1);
         StringBuilder sb = new StringBuilder();
-       for(int i =0; i< n; i++){
+        for (int i = 0; i < n; i++) {
             sb.append(ints[i]).append(" ");
         }
         System.out.println(sb);
     }
 
-    public static void quicksort(int[] arr,int start,int end){
-        if (start < end) { // 종료 조건 체크
-            int part = partition(arr, start, end);
+    public static void quicksort(int[] arr, int start, int end) {
+        int part = partition(arr, start, end);
+        if (start < part - 1) {
             quicksort(arr, start, part - 1);
-            quicksort(arr, part + 1, end);
+        }
+        if (part < end) {
+            quicksort(arr, part, end);
         }
     }
 
-    public static int partition(int[] arr,int start,int end){
-        int pivot = arr[start + end / 2];
-        while(start <= end){
-            while(arr[start] < pivot) start++;
-            while(arr[end] > pivot) end--;
-            if(start <= end){
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+        while (start <= end) {
+            while (arr[start] < pivot) start++;
+            while (arr[end] > pivot) end--;
+            if (start <= end) {
                 int temp = arr[start];
                 arr[start] = arr[end];
                 arr[end] = temp;
