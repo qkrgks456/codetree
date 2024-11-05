7659ca3ddf8c3dd1e6de52014cde5fe878da1df4
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-05 20:14:47 +0900

    Title: 기수 정렬 구현, Time: 1407ms, Memory: 156MB, Status: Passed - CodeTree

--- 241105/기수 정렬 구현/implement-radix-sort.java
+++ 241105/기수 정렬 구현/implement-radix-sort.java
@@ -4,35 +4,39 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> result = new ArrayList();
+        List<Integer> result = new ArrayList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i =0; i< n; i++){
             result.add(Integer.parseInt(st.nextToken()));
         }
-       
-        for(int i = 5; i >= 0; i--){
+
+        for(int i = 0; i < 6; i++){
             // 번호 담을 리스트 만들기
-            List<List<Integer>> list = new ArrayList();
+            List<List<Integer>> list = new ArrayList<>();
             for(int j =0; j < 10; j++){
-                list.add(new ArrayList());
+                list.add(new ArrayList<>());
             }
-            
+
             // 배열의 자리수 맨끝자리 안되면 0으로 넣기
-            for(int j=0; j< result.size();j++){
-                String[] strings = String.valueOf(result.get(j)).split("");
-                if(i <= strings.length - 1){
-                    list.get(Integer.parseInt(strings[i])).add(result.get(j));
-                }else{
-                    list.get(0).add(result.get(j));
+            for (int num : result) {
+                // 문자열 뒤집기
+                String reversed = new StringBuilder(String.valueOf(num))
+                        .reverse()
+                        .toString();
+                String[] strings = reversed.split("");
+
+                if (i < strings.length) {
+                    list.get(Integer.parseInt(strings[i])).add(num);
+                } else {
+                    list.get(0).add(num);
                 }
             }
             // 리스트를 결과값으로
             result.clear();
             for(List<Integer> l : list){
-                for(int num : l){
-                    result.add(num);
-                }
+                result.addAll(l);
             }
+
         }
         StringBuilder sb = new StringBuilder();
         for(int num : result){
