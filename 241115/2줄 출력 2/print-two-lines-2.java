12e66be80afa2471d06d5416bcf36f9d29546c71
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-15 20:50:39 +0900

    Title: 2줄 출력 2, Time: 82ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241115/2줄 출력 2/print-two-lines-2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Do you know Kimchi?").append("\n");
+        sb.append("My Favorite Korean Food Is Kimchi!");
+        System.out.println(sb);
+    }
+}
