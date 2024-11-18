b91abc0003fb358c88dcfd66b16f61ed27f683e8
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-18 19:23:10 +0900

    Title: 문자열 분해, Time: 107ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241118/문자열 분해/string-decomposition.java
+++ 241118/문자열 분해/string-decomposition.java
@@ -1,32 +1,36 @@
 
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         char[] chars = str.toCharArray();
+        char[] pattern = {'a', 'b', 'c', 'd', 'e'};
+        int[] check = new int[chars.length];
         int result = 0;
-        int count = 0;
-        for (char ch : chars) {
-            if (count == 0 && ch == 'a') {
-                count++;
-            } else if (count == 1 && ch == 'b') {
-                count++;
-            } else if (count == 2 && ch == 'c') {
-                count++;
-            } else if (count == 3 && ch == 'd') {
-                count++;
-            } else if (count == 4 && ch == 'e') {
-                count++;
+        while (true) {
+            int patternIndex = 0;
+            boolean flag = false;
+            for (int i = 0; i < chars.length; i++) {
+                if (chars[i] == pattern[patternIndex] && check[i] == 0) {
+                    check[i] = 1;
+                    patternIndex++;
+                    if (patternIndex == pattern.length) {
+                        flag = true;
+                        patternIndex = 0;
+                    }
+                }
             }
-            if (count == 5) {
+            if (flag) {
                 result++;
-                count = 0;
+            } else {
+                break;
             }
         }
+
         System.out.println(result == 0 ? -1 : result);
 
     }
