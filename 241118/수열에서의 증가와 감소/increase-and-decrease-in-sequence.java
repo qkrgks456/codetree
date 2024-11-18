33913da5c09778344862ea103a88e405391b38ae
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-18 23:10:35 +0900

    Title: 수열에서의 증가와 감소, Time: 189ms, Memory: 17MB, Status: Passed - CodeTree

--- 241118/수열에서의 증가와 감소/increase-and-decrease-in-sequence.java
+++ 241118/수열에서의 증가와 감소/increase-and-decrease-in-sequence.java
@@ -1,6 +1,9 @@
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
@@ -9,35 +12,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] ints = new int[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             ints[i] = Integer.parseInt(st.nextToken());
         }
-        // 이분탐색을 배워왔다 해보자 이거야
-        List<Integer> dp = new ArrayList<>();
-        dp.add(ints[0]);
+
+        // 그냥 증가 연속을 구하면 끝이었다 ~
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int max = ints[0];
+        int maxResult = Integer.MIN_VALUE;
         for (int i = 1; i < n; i++) {
-            int target = ints[i];
-            if (dp.get(dp.size() - 1) <= target) {
-                dp.add(target);
-                continue;
+            if (max > ints[i]) {
+                max = ints[i];
+                dp[i] = 1;
+            } else {
+                max = ints[i];
+                dp[i] = dp[i - 1] + 1;
             }
-            int left = 0;
-            int right = dp.size() - 1;
-            int minIdx = dp.size() - 1;
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (dp.get(mid) >= target) {
-                    right = mid - 1;
-                    minIdx = Math.min(mid, minIdx);
-                } else {
-                    left = mid + 1;
-                }
-            }
-            dp.set(minIdx, ints[i]);
+            maxResult = Math.max(maxResult, dp[i]);
         }
-        System.out.println(dp.size());
+        // 감소 연속
+        int min = ints[0];
+        int minResult = Integer.MIN_VALUE;
+        for (int i = 1; i < n; i++) {
+            if (min < ints[i]) {
+                min = ints[i];
+                dp[i] = 1;
+            } else {
+                min = ints[i];
+                dp[i] = dp[i - 1] + 1;
+            }
+            minResult = Math.max(minResult, dp[i]);
+        }
+        System.out.println(Math.max(maxResult, minResult));
+
 
         // 일단 순수 DP로 간다.
         // n^2 시간초과 ㅅㄱㅇ
