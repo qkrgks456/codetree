58b9e427a049920cd4165a44569c421802468a70
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-18 12:40:02 +0900

    Title: 구간 선택하기, Time: 93ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 241118/구간 선택하기/to-select-an-interval.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<Location> list = new ArrayList<>();
+        for(int i =0; i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list.add(new Location(start, end - 1));
+        }
+        Collections.sort(list);
+        int currentEnd = 3;
+        int result = 0;
+        for(Location location : list){
+            if(location.start <= currentEnd && location.end >= currentEnd){
+                currentEnd = location.end;
+                result++;
+            }
+            if(currentEnd >= 220) break;
+        }
+        System.out.println(result);
+    }
+
+     static class Location implements Comparable<Location>{
+            int start;
+            int end;
+            public Location(int start, int end){
+                this.start = start;
+                this.end = end;
+            }
+            @Override
+            public int compareTo(Location o) {
+                if (this.start == o.start) {
+                    return o.end - this.end;
+                }
+                return this.start - o.start;
+            }
+
+     }
+}
