aa89ef7e9501a832348ca0e44dd705bc8f4c388a
박한솔 <76519060+qkrgks456@users.noreply.github.com>
2024-11-22 00:16:10 +0900

    Title: 1, 2, 3 더하기 2, Time: 101ms, Memory: 11MB, Status: Passed - CodeTree

--- 241122/1, 2, 3 더하기 2/1-2-3-plus-2.java
+++ 241122/1, 2, 3 더하기 2/1-2-3-plus-2.java
@@ -1,5 +1,6 @@
-import java.util.*;
-import java.io.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
@@ -13,14 +14,16 @@ public class Main {
         dp[3][1] = 1;
         dp[3][2] = 1;
         dp[3][3] = 1;
+        int MOD = 1000000007;
         for (int i = 4; i <= n; i++) {
             // 1이 끝에 있을때
-            dp[i][1] = ((dp[i - 1][2] % 1000000007) + (dp[i - 1][3] % 1000000007)) % 1000000007;
+            dp[i][1] = ((dp[i - 1][2] + dp[i - 1][3]) % MOD + MOD) % MOD;
             // 2가 끝에 있을때
-            dp[i][2] = ((dp[i - 2][1] % 1000000007) + (dp[i - 2][3] % 1000000007)) % 1000000007;
+            dp[i][2] = ((dp[i - 2][1] + dp[i - 2][3]) % MOD + MOD) % MOD;
             // 3이 끝에 있을때
-            dp[i][3] = ((dp[i - 3][1] % 1000000007) + (dp[i - 3][2] % 1000000007)) % 1000000007;
+            dp[i][3] = ((dp[i - 3][1] + dp[i - 3][2]) % MOD + MOD) % MOD;
         }
-        System.out.println((dp[n][1] + dp[n][2] + dp[n][3]) % 1000000007);
+        int result = ((dp[n][1] + dp[n][2]) % MOD + dp[n][3]) % MOD;
+        System.out.println(result);
     }
 }
